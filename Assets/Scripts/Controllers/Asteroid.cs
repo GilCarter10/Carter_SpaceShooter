fd8258599ecb08096eb04a19cccf3b75ca4f5cc2
GilCarter10 <gilfish1000@gmail.com>
2024-09-24 21:19:04 -0400

    Enemy Movement and Asteroids Working

--- Assets/Scripts/Controllers/Asteroid.cs
+++ Assets/Scripts/Controllers/Asteroid.cs
@@ -1,20 +1,43 @@
+using PlasticPipe.PlasticProtocol.Messages;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Asteroid : MonoBehaviour
 {
-    public float moveSpeed;
-    public float arrivalDistance;
-    public float maxFloatDistance;
+    public float moveSpeed = 100;
+    public float arrivalDistance = 2;
+    public float maxFloatDistance = 0.5f;
+
+    public Vector3 movement;
+    public Vector3 target;
 
     // Start is called before the first frame update
     void Start()
     {
+        movement.x = Random.Range(-maxFloatDistance, maxFloatDistance);
+        movement.y = Random.Range(-maxFloatDistance, maxFloatDistance);
+        target = transform.position + movement;
     }
 
     // Update is called once per frame
     void Update()
     {
+        AsteroidMovement();
+    }
+
+    public void AsteroidMovement()
+    {
+
+        transform.position += movement.normalized * moveSpeed * Time.deltaTime;
+
+        if ((target - transform.position).magnitude < arrivalDistance)
+        {
+            movement.x = Random.Range(-maxFloatDistance, maxFloatDistance);
+            movement.y = Random.Range(-maxFloatDistance, maxFloatDistance);
+            target = transform.position + movement;
+            Debug.Log("new point chosen");
+        }
+
     }
 }
--- Assets/Scripts/Controllers/Enemy.cs
+++ Assets/Scripts/Controllers/Enemy.cs
@@ -3,8 +3,50 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour
 {
+
+    public Vector3 randVector;
+
+    public bool randomize = true;
+
+    private Vector3 oldLocation;
+
+    private Vector3 desiredLocation;
+
+    private Vector3 distanceToGo;
+
     private void Update()
     {
+
+        EnemyMovement(5f, 10f);
+
+    }
+
+    public void EnemyMovement(float speed, float maxComponent)
+    {
+
+        if (randomize)
+        {
+            randVector.x = Random.Range(-maxComponent, maxComponent);
+            randVector.y = Random.Range(-maxComponent, maxComponent);
+            oldLocation = transform.position;
+            randomize = false;
+        }
+
+        desiredLocation = oldLocation + randVector;
+
+        transform.position += randVector.normalized * speed * Time.deltaTime;
+
+        distanceToGo = transform.position - desiredLocation;
+
+        if (distanceToGo.magnitude < 0.01) {
+            randomize = true;
+        }
+
+        if (transform.position.x > 21.5 || transform.position.x < -21.5 || transform.position.y > 10.5 || transform.position.y < -10.5)
+        {
+            randomize = true;
+        }
+
     }
 
 }
